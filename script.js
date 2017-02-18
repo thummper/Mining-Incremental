482ae3b0b6e44b718bdabed244506840d256e4a2
Aron <thummper@hotmail.co.uk>
2017-02-18 19:40:04 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 //Main Object with juicy variable 
 var main = {
     //Stuff here :^)
-    money: 0,
-    moneyIncrement: 15
+    money: 1000000,
+    moneyIncrement: 5
 };
 var mine = {
     copperMinePrice: 500,
     copperMineAmount: 0, 
-    copperMineProduction: 5
+    copperMineProduction: 5,
+    copperOreAmount: 0,
+    maxCopperOre: 1000,
+    copperPerMine: 0, 
+    copperStoragePrice: 10000, 
 };
 //Event Listners when window has loaded.
 window.addEventListener("load", function () {
     document.getElementById("moneyButton").addEventListener("mousemove", function () {
         //Mouse is moving over money button
-        console.log("mousemove on moneyButton");
         main.money += main.moneyIncrement;
+        main.money = Math.floor(main.money);
         Update();
     });
     document.getElementById("copperMine").addEventListener("click", function (e) {
@@ -27,8 +31,40 @@ window.addEventListener("load", function () {
         }
         Buy("CopperMine");
     });
+    
+    document.getElementById("upgradeCopperStorage").addEventListener("click", function (e) {
+        console.log("copperstore");
+        if(e.altKey) {
+            for(i=0; i < 10; i++) {
+                Buy("CopperStorage");
+            }
+            
+        }
+        Buy("CopperStorage");
+        
+    });
+    Update(); 
 });
+setInterval(function() {
+    doMine(); 
+    Update();
+   
+    
+}, 1000);
 //Functions 
+function doMine() {
+    if((mine.copperOreAmount + mine.copperPerMine) >= mine.maxCopperOre) {
+        mine.copperOreAmount = mine.maxCopperOre; 
+        
+    } else {
+        mine.copperOreAmount += mine.copperPerMine;
+    }
+    console.log("Mining, ore: " + mine.copperOreAmount);
+    document.getElementById("copperStorageProgress").value = mine.copperOreAmount;
+    document.getElementById("copperStorageProgress").max = mine.maxCopperOre;
+    
+}
+
 function Buy(item) {
     if (item == "CopperMine") {
         console.log("going to buy");
@@ -38,22 +74,49 @@ function Buy(item) {
             main.money -= mine.copperMinePrice;
             mine.copperMineAmount++;
             mine.copperMinePrice += 550;
+            mine.copperPerMine = mine.copperMineAmount * mine.copperMineProduction;
             Update();
+            
+        }
+    }
+    if(item == "CopperStorage") {
+        console.log("doing buy storage");
+        if(main.money >= mine.copperStoragePrice) {
+            main.money -= mine.copperStoragePrice;
+            mine.maxCopperOre = 2 * mine.maxCopperOre;
+            mine.copperStoragePrice = 3 * mine.copperStoragePrice;
+            Update(); 
         }
     }
 };
+
 //Formats whatever is input
 function Format(input) {
     if (input < 1000) {
         return (input);
     }
     if (input >= 1000000) {
-        return (parseFloat((input / 1000000).toFixed(3)) + " M");
+        return (input / 1000000).toFixed(2) + " M";
     }
     if (input >= 1000) {
-        return (parseFloat((input / 1000).toFixed(3)) + "K");
+        return (input / 1000).toFixed(2) + " K";
     }
 };
+
+//Updates the screen.
+function Update() {
+    document.getElementById("currentMoney").innerHTML = "£" + Format(main.money);
+    document.getElementById("copperMinePrice").innerHTML = "£" + Format(mine.copperMinePrice);
+    document.getElementById("copperMineAmount").innerHTML = Format(mine.copperMineAmount);
+    document.getElementById("copperMineProduction").innerHTML = Format(mine.copperPerMine);
+    document.getElementById("copperOre").innerHTML = Format(mine.copperOreAmount);
+    document.getElementById("maxCopperOre").innerHTML = Format(mine.maxCopperOre);
+    document.getElementById("copperStorageCost").innerHTML = "£" + Format(mine.copperStoragePrice);
+    
+};
+
+
+
 //Handles tabbed panes 
 function Tabs(a) {
     console.log(a);
@@ -70,11 +133,4 @@ function Tabs(a) {
         a.parentNode.children.item(i).classList.remove("actv");
     }
     a.classList.add("actv");
-};
-//Updates the screen.
-function Update() {
-    document.getElementById("currentMoney").innerHTML = "£" + Format(main.money);
-    document.getElementById("copperMinePrice").innerHTML = "£" + Format(mine.copperMinePrice);
-    document.getElementById("copperMineAmount").innerHTML = Format(mine.copperMineAmount);
-    document.getElementById("copperMineProduction").innerHTML = Format(mine.copperMineAmount * mine.copperMineProduction);
 };
